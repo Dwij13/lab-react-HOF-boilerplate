653803777f5a9d64f4f591b6d5bebc8caf6043a4
Dwij13 <dwijjindal2005@gmail.com>
2024-04-25 10:49:10 +0530

    complete

---
+++ hof/src/App.jsx
@@ -0,0 +1,14 @@
+
+import "./App.css"
+import HigherOrderComponent from "./components/HigherOrderFunctions"
+
+function App() {
+
+  return (
+    <>
+      <HigherOrderComponent/>
+    </>
+  )
+}
+
+export default App
